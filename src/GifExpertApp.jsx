9ad3e6a3689dc3efaf8e273765f6f26151bb4e09
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-23 11:36:01 -0500

    agrega una lista de categorias utilizando hook

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,20 @@
 //rafc
+import {useState} from 'react'
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+    console.log('mensaje: ', categories);
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <ol>
+            { categories.map(category => {
+                return <li key={category}>{category}</li>;
+            }) }
+        </ol>
     </>
   )
 }
